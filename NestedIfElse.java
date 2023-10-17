a11cd70c4d3c01ca94919beedd1ae2c0de9b7374
Lancelot <bulanlancelot@gmail.com>
2023-10-18 00:05:39 +0800

    Transfer to Main file

--- NestedIfElse.java
+++ NestedIfElse.java
@@ -22,6 +22,9 @@ public class NestedIfElse{
     System.out.println("\n" + Arrays.toString(tokenized));
     
     //add parsing logic here to analyze structure
+
+    // call check sequence
+    checkSequence(tokenized);
     }
     
     public static String[] tokenize(String[] input) {
@@ -57,4 +60,65 @@ public class NestedIfElse{
         }
         return tokenList.toArray(new String[0]);
     } 
+
+    // check for both balanced parentheses/curly braces and the proper nesting of if-else
+    public static boolean checkSequence(String[] token) {
+        // Initialize
+        boolean isBalanced = true;
+        Stack<String> braceStack = new Stack<String>();
+    
+        // Iterate through the tokens
+        for (int i = 0; i < token.length; i++) {
+            if (token[i].equals("(") || token[i].equals("{")) {
+                // Push opening parentheses and curly braces onto the stack
+                braceStack.push(token[i]);
+            } else if (token[i].equals(")") || token[i].equals("}")) {
+                if (braceStack.empty()) {
+                    // If there's no corresponding opening brace, set isBalanced to false
+                    isBalanced = false;
+                } else {
+                    String top = braceStack.pop();
+                    // Check if the closing brace matches the top of the stack
+                    if ((token[i].equals(")") && !top.equals("(")) || (token[i].equals("}") && !top.equals("{"))) {
+                        isBalanced = false;
+                    }
+                }
+            }
+        }
+    
+        // Check for any leftover opening braces
+        if (!braceStack.empty()) {
+            isBalanced = false;
+        }
+    
+        if (isBalanced) {
+            int ifCount = 0; // Count of "if" tokens
+            int elseCount = 0; // Count of "else" tokens
+            for (String t : token) {
+                if (t.equals("if")) {
+                    ifCount++;
+                } else if (t.equals("else")) {
+                    elseCount++;
+                    if (ifCount < elseCount) {
+                         // If there's an "else" without a matching "if," set isBalanced to false
+                        System.err.println("Incorrect nested if-else sequence");
+                        return false;
+                    }
+                }
+            }
+    
+            // If the counts of "if" and "else" match, it's a correct sequence
+            if (ifCount == elseCount) {
+                System.out.println("Correct for () {} Sequence");
+                return true;
+            } else {
+                System.err.println("Incorrect nested if-else sequence");
+            }
+        } else {
+            System.err.println("Unbalanced parentheses or curly braces");
+        }
+    
+        // add an error counter increment here if needed
+        return false;
+    }
 }
